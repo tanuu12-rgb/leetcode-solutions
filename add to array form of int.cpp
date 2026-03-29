c9032f3708fe89c181e711f0319250f4e08c60b0
tanuu12-rgb <deshmukhtanaya90@gmail.com>
2026-03-29 20:34:30 +0530

    solved with TC:O(nlogn) SC:O(1)

---
+++ add to array form of int.cpp
@@ -0,0 +1,24 @@
+//TC:O(nlogn)
+//SC:O(1)
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        vector<int>result;
+        int n=num.size();
+        for(int i=n-1;i>=0;i--)
+        {
+            if(k!=0)
+            {
+            num[i]+=k;
+            k= num[i]/10;
+            num[i]%=10;
+            }
+        }
+        while (k > 0) {
+            num.insert(num.begin(), k % 10);
+            k /= 10;
+        }
+        return num;
+
+    }
+};
